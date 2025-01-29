8e2378c2935b9394ad243730c50e33c497cc031c
izzzzzzzzzzy <dkriz1662@gmail.com>
2025-01-29 17:00:41 -0500

    added validate_input method, works upon initial testing

--- pokemon_cli.py
+++ pokemon_cli.py
@@ -1,6 +1,6 @@
-from pokemon_admin import db
+# from pokemon_admin import db
 
-KEYS = ['index', 'name', 'type', 'hp', 'stage', 'quit', 'help']
+KEYS = ['index', 'hp', 'stage', 'name', 'type', 'quit', 'help']
 OPS = ['of', '==', '!=', '<=', '>=', '<', '>']
 TYPES = ['normal', 'fire', 'water', 'electric', 'grass', 'ice', 'fighting', 'poison', 
         'ground', 'flying', 'psychic', 'bug', 'rock', 'ghost', 'dragon', 'dark', 'fairy']
@@ -20,6 +20,8 @@ def query_firebase(query):
     
 def take_input():
     while True:
+        valid_query = True
+
         print('> ', end ='')
         input_str = input().lower().strip().split(' ')
 
@@ -29,14 +31,63 @@ def take_input():
         query = []
         subquery = []
         for word in input_str:
-            if word not in {'and', 'or'}:
+            if word not in ['and', 'or']:
                 subquery.append(word)
             else:
                 query.append(subquery)
                 subquery = []
+                query.append([word])
         query.append(subquery)
 
-        print(query)
+        valid_query = validate_input(query)      
+
+        print(query, valid_query)
+
+def validate_input(query):
+    valid_query = True
+
+    for subquery in query:
+        if len(subquery) == 3:
+            key, op, val = subquery
+
+            # checks that the given key value is a valid keyword
+            if key not in KEYS:
+                valid_query = False
+                print(f'Keyword \'{key}\' not recognized')
+            
+            # checks that index, hp, and stage all get ints
+            elif key in KEYS[:3] and op != OPS[0]: 
+                try:
+                    subquery[2] = int(val)
+                except:
+                    valid_query = False
+                    print(f'Keyword \'{key}\' requires an int, found \'{val}\'')
+
+            # checks that name and type are only used with 'of' or '=='
+            elif key in KEYS[3:5] and op != OPS[0]: 
+                if op not in OPS[:2]:
+                    valid_query = False
+                    print(f'Keyword \'{key}\' requires either \'of\' or \'==\' operators, found \'{op}\'')
+
+            # checks that type is always passed a valid type
+            elif key == KEYS[4] and op != OPS[0]:
+                if val not in TYPES:
+                    valid_query = False
+                    print(f'Keyword \'{key}\' requires a valid type (see \'help\'), found \'{val}\'')
+
+            # checks that the given op value is a valid operator
+            if op not in OPS:
+                valid_query = False
+                print(f'Operator \'{op}\' not recognized')
+    
+            # if the 'of' operator is passed, converts val to an int if it is numeric
+            elif op == OPS[0]:
+                try:
+                    subquery[2] = int(val)
+                except:
+                    subquery[2] = val
+
+    return valid_query
 
 take_input()
 print("Goodbye!")
