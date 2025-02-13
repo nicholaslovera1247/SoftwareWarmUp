82cb31c6675f073704ba59f58ce78896c543e571
izzzzzzzzzzy <dkriz1662@gmail.com>
2025-02-12 19:48:37 -0500

    fixed extra whitespace

--- pokemon_admin.py
+++ pokemon_admin.py
@@ -9,18 +9,18 @@ from pokemon_firebase import delete_collection, add_document, authentication
 if len(sys.argv) == 2:
     # Get file name from CLI
     JSON_FILE = sys.argv[1]
-    
+
     # authentication
     auth = authentication()
-    
+
     # Delete existing collection
     delete_collection(auth)
-    
+
     # Read json file
     JSON_FILE = "pokemon.json"
     with open(JSON_FILE, "r", encoding='UTF-8') as file:
         json_data = json.load(file)
-    
+
     # Add documents to Firebase
     try:
         if isinstance(json_data, list):
